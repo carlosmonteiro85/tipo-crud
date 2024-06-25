c57f48a73ec7a898f160c469cb764bcbbe7917da
Carlos Monteiro <85devs@gmail.com>
2024-06-24 23:47:30 -0300

    Refatoração de pacotes

--- jee/src/main/java/com/legado/prototype/crudjee7/conf/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.legado.prototype.crudjee7.conf;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-}
---
+++ jee/src/main/java/com/legado/prototype/crudjee7/core/conf/RestApplication.java
@@ -0,0 +1,8 @@
+package com.legado.prototype.crudjee7.core.conf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
