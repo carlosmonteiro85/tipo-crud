7683867eb03a299151f7185163eb1e32273a0850
Carlos Monteiro <85devs@gmail.com>
2024-07-02 17:04:39 -0300

    Adicionar metodo de listar usuarios e deletar usuario no crud-angular

--- crud-angula/src/app/components/list/list.component.ts
+++ crud-angula/src/app/components/list/list.component.ts
@@ -3,6 +3,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/model/usuario';
 import { DialogService } from 'src/app/service/dialog.service';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { AlertaComponent } from '../alerta/alerta.component';
 
 
 @Component({
@@ -11,23 +13,48 @@ import { DialogService } from 'src/app/service/dialog.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  // usuarios: MockData[] = MOCK_DATA; para usar como mock
-  // usuarios: Usuario[] = []; 
-  usuarios: Usuario[] = MOCK_DATA; 
-  constructor(private dialogService: DialogService) {}
+  usuarios: Usuario[] = [];
+  alerta = new AlertaComponent();
+
+  constructor(
+    private dialogService: DialogService,
+    private usuariosService: UsuarioService
+  ) {}
   
   ngOnInit(): void {
-    // Inicialmente, definimos os usuários com MOCK_DATA
+    this.findAll();
+  }
+
+  findAll(){
+    this.usuariosService.findAll().subscribe(response => {
+      this.usuarios = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  delete(id: string){
+    this.usuariosService.delete(id).subscribe(response => {
+      this.findAll();
+
+      this.alerta.icon = 'success';
+      this.alerta.text = 'Usuário deletado com sucesso!';
+      this.alerta.showAlert();
+
+    }, error => {
+      this.alerta.icon = 'error';
+      this.alerta.text = 'Erro ao deletar usuário!';
+      this.alerta.showAlert();
+      console.log(error);
+    });
   }
   
   showDialog(usuario: Usuario) {
     console.log('Show dialog for user:', usuario);
     
-    // Exemplo de como chamar o diálogo de confirmação usando o serviço
     this.dialogService.showAlert().then(result => {
       if (result) {
-        console.log(`Usuário ${usuario.nome} confirmou a exclusão`);
-        // Lógica para excluir o usuário aqui
+        this.delete(`${usuario.id}`)
       } else {
         console.log(`Usuário ${usuario.nome} cancelou a exclusão`);
         // Lógica para não excluir o usuário aqui
@@ -37,35 +64,3 @@ export class ListComponent implements OnInit {
     });
   }
 }
-
-// interface MockData {
-//   id: number;
-//   nome: string;
-//   cpf: string;
-//   telefone: string;
-//   email: string;
-// }
-
-const MOCK_DATA: Usuario[] = [
-  {
-    id: 1,
-    nome: 'João',
-    cpf: '123.456.789-10',
-    telefone: '1234-5678',
-    email: 'joao.com'
-  },
-  {
-    id: 2,
-    nome: 'Maria',
-    cpf: '123.456.789-10',
-    telefone: '1234-5678',
-    email: 'maria.com'
-  },
-  {
-    id: 3,
-    nome: 'José',
-    cpf: '123.456.789-10',
-    telefone: '1234-5678',
-    email: 'jose.com'
-  },
-];
--- crud-angula/src/app/service/usuario.service.ts
+++ crud-angula/src/app/service/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../model/usuario';
 import { API_CONFIG } from '../config/api.config';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,16 @@ export class UsuarioService {
       observe:'response',
       responseType: 'text'
     })  
+  }
+
+  findAll(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${API_CONFIG.baseUrl}/usuarios`);
+  }
+
+  delete(id: string){
+    return this.http.delete(`${API_CONFIG.baseUrl}/usuarios/${id}`, {
+      observe:'response',
+      responseType: 'text'
+    })
   }
 }
